ecad0e1250e8b082f42ae61a7230dd893c70b6a7
srikanth <ks.srikanth121@gmail.com>
2023-02-25 09:39:29 +0530

    test ec2

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami = "ami-0a017d8ceb274537d"
   instance_type="t3.micro"
-  security_groups = ["sg-00fdd7936b7707f53"]
+  vpc_security_group_ids = ["sg-00fdd7936b7707f53"]
   tags ={
     Name ="test"
   }
