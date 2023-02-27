f92d4cfb2b6d9dbeef59202e516342b5a45a2d69
srikanth <ks.srikanth121@gmail.com>
2023-02-27 19:43:25 +0530

    test ec2

--- 04-vars/vars.tf
+++ 04-vars/vars.tf
@@ -17,3 +17,28 @@ variable "cli" {}
 output "cli" {
   value = var.cli
 }
+
+variable "sample5" {
+  default = "Hello"
+}
+
+variable "sample6" {
+  default =  [
+    "Hello",
+    1000,
+    true,
+    "World"
+  ]
+}
+
+variable "sample7" {
+  default =  {
+    string = "Hello",
+    number = 100,
+    boolean = true
+  }
+}
+
+output "types" {
+  value = "Variable sample5 - ${var.sample5}, first value in list - ${var.sample6[0]},Boolean value in map - ${var.sample7["boolean"]}"
+}
